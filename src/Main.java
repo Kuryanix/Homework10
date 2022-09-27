ad42a77546997d8cbbdd37d52c32ef046858ceef
Максим <99maks@bk.ru>
2022-09-27 18:10:27 +0300

    ДЗ 10 готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        // Задача 2
+        String fullNameReport = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameReport);
+
+        // Задача 3
+        String fullName3 = "Иванов Семён Семёнович";
+        String fullNameCorrect = fullName3.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullNameCorrect);
 
     }
 }
